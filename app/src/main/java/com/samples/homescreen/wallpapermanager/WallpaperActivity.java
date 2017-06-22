9eb0e233123df65e0f4624dfc0c55e35c3bb9ee5
java2019 <java_2019@mail.ru>
2017-06-22 10:40:17 +0300

    test1

--- app/src/main/java/com/samples/homescreen/wallpapermanager/WallpaperActivity.java
+++ app/src/main/java/com/samples/homescreen/wallpapermanager/WallpaperActivity.java
@@ -1,13 +1,31 @@
 package com.samples.homescreen.wallpapermanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.WallpaperManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class WallpaperActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class WallpaperActivity extends Activity implements View.OnClickListener{
+
+    private WallpaperManager wallpaperManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallpaper);
+    }
+
+    @Override
+    public void onClick(View view) {
+        wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+        try{
+            wallpaperManager.setResource(R.drawable.wallpaper);
+            Toast.makeText(this, "Wallpaper has been changed", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
